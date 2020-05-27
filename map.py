a28546626972dca42cf8b9e94f63a9f568f90b37
zainbitsi <hello.zain11@gmail.com>
2020-05-28 02:21:59 +0530

    Dynamic Popup Names | index.html Added

--- map.py
+++ map.py
@@ -10,13 +10,14 @@ data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
+name = list(data["NAME"])
 
 map = folium.Map(location=[42.554492, -114.475976], zoom_start=5, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt,ln], popup=folium.Popup(str(el), parse_html=True), icon=folium.Icon(color='red')))
+for lt, ln, el, nm in zip(lat, lon, elev, name):
+    fg.add_child(folium.Marker(location=[lt,ln], popup=folium.Popup(str(nm) + "\n" + str(el) + "m", parse_html=True), icon=folium.Icon(color='red')))
 
 map.add_child(fg)
 
