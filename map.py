ae0af3bf21845cf6b2a59aa14395091f25a650d8
zainbits <hello.zain11@gmail.com>
2020-05-28 01:36:32 +0530

    Dynamic Popup (Shows Elevation)

--- map.py
+++ map.py
@@ -9,13 +9,14 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[42.554492, -114.475976], zoom_start=5, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt,ln], popup="This is Volcano", icon=folium.Icon(color='red')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt,ln], popup=folium.Popup(str(el), parse_html=True), icon=folium.Icon(color='red')))
 
 map.add_child(fg)
 
