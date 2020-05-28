a78378657bde8e492841768d39e1f451676cd472
zainbitsi <hello.zain11@gmail.com>
2020-05-28 18:48:54 +0530

    Coloring Based On Population

--- map.py
+++ map.py
@@ -27,7 +27,9 @@ fg = folium.FeatureGroup(name="My Map")
 for lt, ln, el, nm in zip(lat, lon, elev, name):
     fg.add_child(folium.CircleMarker(location=[lt,ln], popup=folium.Popup(str(nm) + "\n" + str(el) + "m", parse_html=True), color='black', fill_color=color_produce(el), fill_opacity=0.7))
     
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))    
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function = lambda x : {'fillColor' : 'green' if x['properties']['POP2005'] < 10000000 
+else 'orange' if 10000000 <= x['properties']['POP2005'] <= 20000000 else 'red'}))    
 
 map.add_child(fg)
 
